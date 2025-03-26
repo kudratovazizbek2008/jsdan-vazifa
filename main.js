ed3093e8c5966350c0b764d8cb606229170258f7
kudratovazizbek2008 <kudratovazizbek2008@gmail.com>
2025-03-26 10:20:43 +0500

    creat uyga 1

---
+++ main.js
@@ -0,0 +1,38 @@
+// // 1-masala
+// let number = 1
+// let numbers = 100
+//
+// for (let i = number; i <= numbers; i++) {
+//     console.log(i)
+// }
+
+// 2-masala
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d;
+//
+// d=a
+// a=b
+// b=c
+// c=d
+// console.log(a,b,c);
+
+
+
+// 3-masala
+// let A = 3
+// let B = 7;
+// let sum = ((B - A + 1) * (A + B)) / 2;
+// console.log(sum);
+
+
+
+let N = 5;
+let numbers = 1;
+
+for (let i = 1; i <= N; i++) {
+    numbers *= i;
+}
+
+console.log(numbers);
