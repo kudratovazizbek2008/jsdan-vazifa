b09a29f9e2446fea9b1aca4101ccf0a8895cd9b5
kudratovazizbek2008 <kudratovazizbek2008@gmail.com>
2025-04-02 08:57:12 +0500

    creat uyga 1

--- main.js
+++ main.js
@@ -1,140 +1,73 @@
-// 1-masala
-// function main(num) {
-//     if (num > 0) {
-//         num += 1;
+// For1
+// function main(k, n) {
+//     for (let i = 0; i < n; i++) {
+//         console.log(k);
 //     }
-//     console.log(num);
 // }
-// main(5)
-// main(-5)
+// main(2,4)
 
-// 2-masala
-// function main(a) {
-//     if (a > 0) {
-//         a += 1;
-//     } else {
-//         a -= 2;
-//     }
-//     console.log(a);
-// }
-// main(5)
-// main(-5)
-
-// 3-masala
-// function main(b) {
-//     if (b > 0) {
-//         b += 1;
-//     } else if (b < 0) {
-//         b -= 2;
-//     } else {
-//         b = 10;
-//     }
-//     console.log(b);
-// }
-// main(4);
-// main(-2);
-// main(0);
-
-// 4-masala
-// function main(a, b, c) {
-//     let d = 0;
-//     if (a > 0) d++;
-//     if (b > 0) d++;
-//     if (c > 0) d++;
-//
-//     console.log(d);
-// }
-// main(3, -1, 7);
-// main(-5, -2, -8);
-// main(1, 2, 3);
-
-// 5-masala
-// function main(a, b, c) {
-//     let d = 0;
-//     let e = 0;
-//     if (a < 0) e++;
-//     if (b < 0) e++;
-//     if (c < 0) e++;
-//     if (a > 0) d++;
-//     if (b > 0) d++;
-//     if (c > 0) d++;
-//
-//     console.log(`${d} musbat ${e} manfiy`);
-// }
-// main(3, -1, 7);
-// main(-5, -2, -8);
-// main(1, 2, 3);
-
-// 6-masala
+// For2
 // function main(a, b) {
-//     if (a > b) {
-//         return '${a} kattaroq';
-//     } else if (b > a) {
-//         return '${b} kattaroq';
-//     } else {
-//         return "Ikkalasi teng";
+//     for (let i = a; i <= b; i++) {
+//         console.log(i);
 //     }
+//     console.log(b - a + 1);
 // }
-//
-// let son1 = parseInt(prompt("Birinchi sonni kiriting:"));
-// let son2 = parseInt(prompt("Ikkinchi sonni kiriting:"));
-//
-// alert(main(son1, son2));
+// main(2,4)
 
-// 7-masala
+// For3
 // function main(a, b) {
-//     if (a < b) {
-//         return "1-son kichik ";
-//     } else if (b < a) {
-//         return "2-son kichik";
-//     } else {
-//         return "Ikkalasi teng, tartib raqami yo'q";
+//     for (let i = b; i >= a; i--) {
+//         console.log(i);
 //     }
-//
+//     console.log(b - a + 1);
 // }
-// let son1 = parseInt(prompt("Birinchi sonni kiriting:"));
-// let son2 = parseInt(prompt("Ikkinchi sonni kiriting:"));
-//
-// alert(main(son1, son2));
+// main(2,4)
 
-// 8-masala
+// For4
+// function main(price) {
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(`${i} kg = ${i * price}`);
+//     }
+// }
+// main(2,4)
 
+// For7
 // function main(a, b) {
-//     if (a > b) {
-//         console.log(a, b);
-//     } else {
-//         console.log(b, a);
+//     let sum = 0;
+//     for (let i = a; i <= b; i++) {
+//         sum += i;
 //     }
+//     console.log(sum);
 // }
-// let son1 = parseInt(prompt("Birinchi sonni kiriting:"));
-// let son2 = parseInt(prompt("Ikkinchi sonni kiriting:"));
-// main(son1, son2);
+// main(2,4)
 
-// 9-masala
+// For8
 // function main(a, b) {
-//     if (a > b) {
-//         let d = a;
-//         a = b;
-//         b = d;
+//     let number = 1;
+//     for (let i = a; i <= b; i++) {
+//         number *= i;
 //     }
-//     console.log("a =", a, "b =", b);
+//     console.log(number);
 // }
-// let a = parseFloat(prompt("a ni kiriting:"));
-// let b = parseFloat(prompt("b ni kiriting:"));
-// main(a, b);
+// main(2,4)
 
-// 10-masla
+// For9
 // function main(a, b) {
-//     if (a !== b) {
-//         a=a+b;
-//         b = a;
-//     } else {
-//         a = 0;
-//         b = 0;
+//     let sum = 0;
+//     for (let i = a; i <= b; i++) {
+//         sum += i * i;
 //     }
-//     console.log("a =", a, "b =", b);
+//     console.log(sum);
 // }
-// let a = parseFloat(prompt("A ni kiriting:"));
-// let b = parseFloat(prompt("B ni kiriting:"));
-// main(a, b);
+// main(2,4)
 
+// For10
+function main(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += 1 / i;
+    }
+    console.log(sum);
+}
+main(2,4)
