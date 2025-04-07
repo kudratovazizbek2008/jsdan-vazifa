e8955e8856937e06e193b978dd06409f47b56130
kudratovazizbek2008 <kudratovazizbek2008@gmail.com>
2025-04-07 12:59:16 +0500

    creat uyga_1

---
+++ index.js
@@ -0,0 +1,29 @@
+// let course = {
+//     name:"Foundation",
+//     type: "N2",
+//     text: "Dasturlashni yangi boshlovchilar uchun asosiy tushuncha va tamoyillar. IT dan xabari umuman bo’lmagan o’quvchilarga tavsiya.",
+//     price: 12000,
+//     fullname: function (){
+//         return this.name + this.type
+// }
+//
+// }
+// console.log(course.fullname())
+
+let course =[
+    {
+        name: "Foundation",
+        type: "N2",
+        text: "Dasturlashni yangi boshlovchilar uchun asosiy tushuncha va tamoyillar. IT dan xabari umuman bo’lmagan o’quvchilarga tavsiya.",
+        price: 12000,
+    },
+    {
+        name: "Foundation",
+        type: "N2",
+        text: "Dasturlashni eski boshlovchilar uchun asosiy tushuncha va tamoyillar. IT dan xabari umuman bo’lmagan o’quvchilarga tavsiya.",
+        price: 12000,
+
+
+    }
+]
+console.log(course[0].name)
